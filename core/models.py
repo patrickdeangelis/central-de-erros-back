e3d758a036046feb544813dcaa7d3af5f102e3ea
patrick <patrick.angelis@hotmail.com>
2020-07-17 21:19:25 -0300

    Add Agent and Event models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator, EmailValidator, validate_ipv4_address
+import datetime
 
-# Create your models here.
+
+class Agent(models.Model):
+    class Enviroments(models.TextChoices):
+        PRODUCTION = ("PRODUCTION", "PRODUCTION")
+        HOMOLOGATION = ("HOMOLOGATION", "HOMOLOGATION")
+        DEV = ("DEV", "DEV")
+
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
+    address = models.GenericIPAddressField(validators=[validate_ipv4_address], null=True)
+    status = models.BooleanField(default=False)
+    env = models.CharField(max_length=20, choices=Enviroments.choices)
+    version = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Event(models.Model):
+    class Levels(models.TextChoices):
+        CRITICAL = ("CRITICAL", "CRITICAL")
+        DEBUG = ("DEBUG", "DEBUG")
+        ERROR = ("ERROR", "ERROR") 
+        WARNING = ("WARNING", "WARNING")
+        INFO = ("INFO", "INFO")
+
+    level = models.CharField(max_length=20, choices=Levels.choices)
+    data = models.TextField(max_length=500)
+    agent = models.OneToOneField(Agent, on_delete=models.PROTECT)
+    shelved = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.level +' in ' + self.agent.name
+
+    class Meta:
+        ordering = ['date']
+
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+from django.core.exceptions import ValidationError
+from .models import Agent, Event
+from django.contrib.auth.models import User
+
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="Jose", email="jose@gmail.com", password="xxxxxxxxxxxxxxxxxxxxxxx"
+        )
+        self.agent = Agent.objects.create(
+            name="Machine1",
+            address="192.168.1.1",
+            status=True,
+            env=Agent.Enviroments.DEV,
+            version="1.1.1",
+        )
+        Event.objects.create(
+            level=Event.Levels.CRITICAL,
+            data="django.core.exceptions.ValidationError",
+            agent=self.agent,
+            shelved=False,
+        )
+
+    def test_user_exists(self):
+        user = User.objects.get(email="jose@gmail.com")
+        self.assertEqual(user.username, "Jose")
+
+    def test_agent_exists(self):
+        agent = Agent.objects.get(name="Machine1")
+        self.assertEqual(agent.name, "Machine1")
+
+    def test_event_exists(self):
+        event = Event.objects.get(level=Event.Levels.CRITICAL)
+
+        self.assertEqual(event.data, "django.core.exceptions.ValidationError")
+
+    def test_should_raise_validation_error_on_invalid_level(self):
+        event = Event(
+            level="INVALID_LEVEL",
+            data="django.core.exceptions.ValidationError",
+            agent=self.agent,
+            shelved=False,
+        )
+        self.assertRaises(ValidationError, event.full_clean)
+
