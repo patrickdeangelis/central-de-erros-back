903262fefd4a278c44fbcb4aa94e36cf9ae741e5
patrick <patrick.angelis@hotmail.com>
2020-07-18 21:49:58 -0300

    Update models fields

--- core/models.py
+++ core/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinLengthValidator, EmailValidator, validate_ipv4_address
+from django.core.validators import (
+    MinLengthValidator,
+    EmailValidator,
+    validate_ipv4_address,
+)
 import datetime
 
 
@@ -10,37 +14,38 @@ class Agent(models.Model):
         HOMOLOGATION = ("HOMOLOGATION", "HOMOLOGATION")
         DEV = ("DEV", "DEV")
 
-    name = models.CharField(max_length=50)
-    user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
-    address = models.GenericIPAddressField(validators=[validate_ipv4_address], null=True)
-    status = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    address = models.GenericIPAddressField(
+        validators=[validate_ipv4_address], null=True
+    )
     env = models.CharField(max_length=20, choices=Enviroments.choices)
     version = models.CharField(max_length=5)
 
     def __str__(self):
-        return self.name
+        return self.user.username
 
     class Meta:
-        ordering = ['name']
+        ordering = ["user"]
 
 
 class Event(models.Model):
     class Levels(models.TextChoices):
         CRITICAL = ("CRITICAL", "CRITICAL")
         DEBUG = ("DEBUG", "DEBUG")
-        ERROR = ("ERROR", "ERROR") 
+        ERROR = ("ERROR", "ERROR")
         WARNING = ("WARNING", "WARNING")
         INFO = ("INFO", "INFO")
 
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
     level = models.CharField(max_length=20, choices=Levels.choices)
-    data = models.TextField(max_length=500)
-    agent = models.OneToOneField(Agent, on_delete=models.PROTECT)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
     shelved = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.level +' in ' + self.agent.name
+        return self.level + " in " + str(self.agent)
 
     class Meta:
-        ordering = ['date']
+        ordering = ["date"]
 
