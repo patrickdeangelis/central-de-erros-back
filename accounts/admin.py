bf1bccff41b5ac89e85f25558c67f9274ad68406
patrick <patrick.angelis@hotmail.com>
2020-07-20 16:32:07 -0300

    Disable some admin functionalities

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import UserAccount
 
-admin.site.register(UserAccount, UserAdmin)
+admin.site.register(UserAccount)
